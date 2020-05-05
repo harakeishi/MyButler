489654905bcbbad03401596ba7050c2ee70bb505
hara keishi <hara.for.work@gmail.com>
2020-05-05 15:28:34 +0900

    server側にトークン発行とユーザー登録・認証機能を実装

---
+++ server/app/controllers/memos_controller.rb
@@ -0,0 +1,60 @@
+class MemosController < ApplicationController
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+  before_action :set_memo, only: [:show, :update, :destroy]
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token,options|
+      auth_user = User.find_by(token: token)
+      auth_user != nil ? true : false
+    end
+   end
+  # GET /memos
+  def index
+    @memos = Memo.all
+
+    render json: @memos
+  end
+
+  # GET /memos/1
+  def show
+    render json: @memo
+  end
+
+  # POST /memos
+  def create
+    @memo = Memo.new(memo_params)
+
+    if @memo.save
+      render json: @memo, status: :created, location: @memo
+    else
+      render json: @memo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /memos/1
+  def update
+    if @memo.update(memo_params)
+      render json: @memo
+    else
+      render json: @memo.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /memos/1
+  def destroy
+    @memo.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_memo
+      @memo = Memo.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def memo_params
+      params.require(:memo).permit(:title, :content)
+    end
+end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'login/login'
+  resources :users
+  resources :memos
+  post 'login/login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
